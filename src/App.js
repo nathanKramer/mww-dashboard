02aff20afa3e1e974f098d6a9258e715270ecd61
Nathan Kramer <nathan.l.kramer@gmail.com>
2018-11-11 13:26:17 +1100

    Implement a basic table

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { withStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+
 import './App.css';
 
+const CustomTableCell = withStyles(theme => ({
+  root: {
+    background: "#282c34"
+  },
+  head: {
+    color: theme.palette.common.white,
+  },
+  body: {
+    fontSize: 14,
+    color: theme.palette.common.white
+  },
+}))(TableCell);
+
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { servers: [] };
+  }
+
+  componentDidMount() {
+    fetch('https://mww-reborn-api.herokuapp.com/v1/servers')
+    .then(response => response.json())
+      .then(data => this.setState({ servers: data }));
+  }
+
+  renderServers() {
+    if (!this.state.servers) {
+      return null;
+    }
+
+    return this.state.servers.map(server => {
+      return (
+        <TableRow key={server.name}>
+          <CustomTableCell key={`${server.name}-name`}>{server.name}</CustomTableCell>
+          <CustomTableCell key={`${server.name}-players`}>{server.players}</CustomTableCell>
+          <CustomTableCell key={`${server.name}-join`}><a href={`steam://${server.server_addr}`}>join on steam...</a></CustomTableCell>
+        </TableRow>
+      )
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>MWW Reborn</h1>
+          <ul>
+            <li><a href="http://steamcommunity.com/sharedfiles/filedetails/?id=904845972">Getting started guide</a></li>
+            <li><a href="https://timer00.github.io/ReviveWizardWars/Releases/mww-client-installer-2.4.0.exe">Latest Client Installer</a></li>
+          </ul>
+          <Table className="server-table">
+            <TableHead>
+              <TableRow>
+                <CustomTableCell>Name</CustomTableCell>
+                <CustomTableCell>Players</CustomTableCell>
+                <CustomTableCell>Join Link</CustomTableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {this.renderServers()}
+            </TableBody>
+          </Table>
         </header>
       </div>
     );
